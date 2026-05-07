d17605eb25dbc8fb5c318297a33aea0f03d53ed4
Clover You <hi@clovu.me>
2026-05-08 03:17:51 +0800

    fix: remove unused InputProps type export

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,7 +2,6 @@ export { Button } from './button'
 export type { ButtonProps } from './button'
 
 export { Input } from './input'
-export type { InputProps } from './input'
 
 export * from './card'
 
