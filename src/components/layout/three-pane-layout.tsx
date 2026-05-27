1d730e075b2f342ea06b97f0c191110739422743
Clover You <hi@clovu.me>
2026-05-27 09:46:44 +0800

    refactor: use react-resizable-panels for three-pane layout
    Replace fixed-width aside with resizable panels supporting pixel-based
    sizing, enabling user-adjustable list pane width.

--- src/components/layout/three-pane-layout.tsx
+++ src/components/layout/three-pane-layout.tsx
@@ -1,10 +1,12 @@
 import { cn } from '@/lib/utils'
+import { Group, Panel, Separator } from 'react-resizable-panels'
 
 interface ThreePaneLayoutProps {
   list: React.ReactNode
   children: React.ReactNode
   listClassName?: string
   contentClassName?: string
+  disableResize?: boolean
 }
 
 export function ThreePaneLayout({
@@ -12,25 +14,31 @@ export function ThreePaneLayout({
   children,
   listClassName,
   contentClassName,
+  disableResize,
 }: ThreePaneLayoutProps) {
   return (
-    <div className="flex h-full min-h-0 min-w-0 flex-1 overflow-hidden">
-      <aside
-        className={cn(
-          'w-72 bg-[#13131d] border-r border-border flex flex-col shrink-0',
-          listClassName,
-        )}
-      >
-        {list}
-      </aside>
-      <section
-        className={cn(
-          'flex min-w-0 flex-1 flex-col bg-background',
-          contentClassName,
-        )}
-      >
-        {children}
-      </section>
-    </div>
+    <Group className="flex h-full min-h-0 min-w-0 flex-1 overflow-hidden" disabled={disableResize}>
+      <Panel groupResizeBehavior="preserve-pixel-size" defaultSize={288} maxSize={400} minSize={200}>
+        <aside
+          className={cn(
+            'size-full bg-[#13131d] border-r border-border flex flex-col shrink-0',
+            listClassName,
+          )}
+        >
+          {list}
+        </aside>
+      </Panel>
+      <Separator />
+      <Panel>
+        <section
+          className={cn(
+            'size-full flex min-w-0 flex-1 flex-col bg-background',
+            contentClassName,
+          )}
+        >
+          {children}
+        </section>
+      </Panel>
+    </Group>
   )
 }
